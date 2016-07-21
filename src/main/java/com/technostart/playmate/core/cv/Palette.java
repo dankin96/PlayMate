d19ff762790ef28cc3f629fafccf85694986cea6
Ruslan <vergilijus@ya.ru>
2016-07-21 23:05:55 +0300

    Исправлен Palette

--- src/main/java/com/technostart/playmate/core/cv/Palette.java
+++ src/main/java/com/technostart/playmate/core/cv/Palette.java
@@ -5,12 +5,24 @@ import org.opencv.core.Scalar;
 import java.util.Arrays;
 import java.util.List;
 
-public class Color {
+public class Palette {
     public static final Scalar BLUE = new Scalar(255, 0, 0);
     public static final Scalar GREEN = new Scalar(0, 255, 0);
     public static final Scalar RED = new Scalar(0, 0, 255);
+    public static final Scalar YELLOW = new Scalar(0, 255, 255);
+    public static final Scalar PURPLE = new Scalar(255, 0, 255);
+    public static final Scalar BG = new Scalar(255, 255, 0);
     public static final Scalar BLACK = new Scalar(0, 0, 0);
     public static final Scalar WHITE = new Scalar(255, 255, 255);
 
-    public static final List<Scalar> palette = Arrays.asList(BLUE, GREEN, RED);
+    public static final List<Scalar> palette = Arrays.asList(BLUE, GREEN, RED, YELLOW, PURPLE, BG);
+    private static int colorIdx = 0;
+
+    public static Scalar getNextColor() {
+        colorIdx++;
+        if (colorIdx >= palette.size()) colorIdx = 0;
+        return palette.get(colorIdx);
+    }
+
+
 }
