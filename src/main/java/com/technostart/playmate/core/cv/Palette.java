f9c2137c8044dffa1bc0aa8a2357941c90376687
Ruslan <vergilijus@ya.ru>
2016-07-25 20:46:10 +0300

    Обновлена палитра

--- src/main/java/com/technostart/playmate/core/cv/Palette.java
+++ src/main/java/com/technostart/playmate/core/cv/Palette.java
@@ -5,6 +5,7 @@ import org.opencv.core.Scalar;
 import java.util.Arrays;
 import java.util.List;
 
+@SuppressWarnings("WeakerAccess")
 public class Palette {
     public static final Scalar BLUE = new Scalar(255, 0, 0);
     public static final Scalar GREEN = new Scalar(0, 255, 0);
@@ -12,10 +13,13 @@ public class Palette {
     public static final Scalar YELLOW = new Scalar(0, 255, 255);
     public static final Scalar PURPLE = new Scalar(255, 0, 255);
     public static final Scalar BG = new Scalar(255, 255, 0);
+    public static final Scalar ORANGE = new Scalar(0, 69, 255);
+    public static final Scalar PINK = new Scalar(147, 20, 255);
+    public static final Scalar BROWN = new Scalar(19, 69, 139);
     public static final Scalar BLACK = new Scalar(0, 0, 0);
     public static final Scalar WHITE = new Scalar(255, 255, 255);
 
-    public static final List<Scalar> palette = Arrays.asList(BLUE, GREEN, RED, YELLOW, PURPLE, BG);
+    public static final List<Scalar> palette = Arrays.asList(BLUE, GREEN, RED, YELLOW, PURPLE, BG, ORANGE, PINK, BROWN);
     private static int colorIdx = 0;
 
     public static Scalar getNextColor() {
@@ -23,6 +27,5 @@ public class Palette {
         if (colorIdx >= palette.size()) colorIdx = 0;
         return palette.get(colorIdx);
     }
-
-
+    
 }
