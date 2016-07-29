e00573df4ba8ad520f947b5f9eb7831841d89346
Ruslan <vergilijus@ya.ru>
2016-07-29 20:58:04 +0300

    Добавлен метод getRandomColor в палитру

--- src/main/java/com/technostart/playmate/core/cv/Palette.java
+++ src/main/java/com/technostart/playmate/core/cv/Palette.java
@@ -4,6 +4,7 @@ import org.opencv.core.Scalar;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 @SuppressWarnings("WeakerAccess")
 public class Palette {
@@ -27,5 +28,23 @@ public class Palette {
         if (colorIdx >= palette.size()) colorIdx = 0;
         return palette.get(colorIdx);
     }
-    
+
+    public static Scalar getRandomColor() {
+        return getRandomColor(0);
+    }
+
+    /**
+     * @param offset чем меньше тем ярче цвета и больше коллизий.
+     * @return случайный цвет в формате BGR.
+     */
+    public static Scalar getRandomColor(int offset) {
+        Random random = new Random();
+        int delta = 255 - offset;
+
+        int blue = random.nextInt(delta) + offset;
+        int green = random.nextInt(delta) + offset;
+        int red = random.nextInt(delta) + offset;
+        return new Scalar(blue, green, red);
+    }
+
 }
