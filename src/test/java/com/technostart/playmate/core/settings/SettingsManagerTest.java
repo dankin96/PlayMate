cdb756cc19fff184c1fa137745a9d9b8ac318332
Ruslan <vergilijus@ya.ru>
2016-08-14 07:43:12 +0300

    Заготовка тестов настроек

--- src/test/java/com/technostart/playmate/core/settings/SettingsManagerTest.java
+++ src/test/java/com/technostart/playmate/core/settings/SettingsManagerTest.java
@@ -6,15 +6,31 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class SettingsManagerTest {
+    private TestClass testClass;
+    private SettingsManager manager;
+
     @Before
     public void setUp() throws Exception {
+        testClass = new TestClass();
+        manager = new SettingsManager();
+    }
+
+    @Test
+    public void toSettings() throws Exception {
+        manager.toSettings(testClass);
+        assertEquals(123, manager.getInt("int123"));
+        assertEquals(12.34, manager.getDouble("double1"), 0);
+        assertEquals(true, manager.getBoolean("bool1"));
+    }
+
+    @Test
+    public void fromSettings() throws Exception {
 
     }
 
     @Test
     public void intFromJson() throws Exception {
         String json = "{\"i1\":{\"type\":\"Integer\",\"value\":1},\"i2\":{\"type\":\"Integer\",\"value\":2}}";
-        SettingsManager manager = new SettingsManager();
         manager.fromJson(json);
         assertEquals(manager.getInt("i1"), 1);
         assertEquals(manager.getInt("i2"), 2);
@@ -23,7 +39,6 @@ public class SettingsManagerTest {
     @Test
     public void boolFromJson() throws Exception {
         String json = "{\"b1\":{\"type\":\"Boolean\",\"value\":true}}";
-        SettingsManager manager = new SettingsManager();
         manager.fromJson(json);
         assertEquals(manager.getBoolean("b1"), true);
     }
@@ -31,14 +46,12 @@ public class SettingsManagerTest {
     @Test
     public void stringFromJson() throws Exception {
         String json = "{\"s1\":{\"type\":\"String\",\"value\":\"hello\"}}";
-        SettingsManager manager = new SettingsManager();
         manager.fromJson(json);
         assertEquals(manager.getString("s1"), "hello");
     }
 
     @Test
     public void toJson() throws Exception {
-        SettingsManager manager = new SettingsManager();
         manager.mockProperty();
         String json = manager.toJson();
         System.out.println(json);
