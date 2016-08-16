bf564f20b3839cf1124b184e3e15d54ea4ba380e
Ruslan <vergilijus@ya.ru>
2016-08-17 02:25:16 +0300

    Добавлена проверка углов в апроксимации

--- src/main/java/com/technostart/playmate/core/cv/Utils.java
+++ src/main/java/com/technostart/playmate/core/cv/Utils.java
@@ -271,45 +271,67 @@ public class Utils {
         return hullContour;
     }
 
+    public static double getAngle(Point a, Point b, Point c) {
+        Point ab = new Point(b.x - a.x, b.y - a.y);
+        Point bc = new Point(b.x - c.x, b.y - c.y);
+        //  a.x*b.y - a.y*b.x, a.x*b.x + a.y*b.y
+        double angle = Math.toDegrees(Math.atan2(ab.x * bc.y - ab.y * bc.x, ab.x * bc.x + ab.y * bc.y));
+        return angle >= 0 ? angle : -angle;
+    }
+
     public static List<Point> approximate(MatOfPoint temp, int edgesNumber) {
         List<Point> listOfPoints = temp.toList();
         listOfPoints = new LinkedList<>(listOfPoints);
         // убираем итеративно стороны
-        while (listOfPoints.size() != edgesNumber) {
+        for (int i = 0; i < listOfPoints.size() - edgesNumber || listOfPoints.size() == edgesNumber; i++) {
+//        while (listOfPoints.size() != edgesNumber) {
             int listOfPointsSize = listOfPoints.size();
             int lastIdx = listOfPointsSize - 1;
             double min_distance = Double.MAX_VALUE;
-            int beginPointIdx = -1;
-            int endPointIdx = -1;
+
+            Point point1 = null;
+            Point point2 = null;
+            Point point3 = null;
+            Point point4 = null;
+
+            int point2idx = -1;
+            int point3idx = -1;
+
             for (int j = 0; j < listOfPoints.size(); j++) {
-                int curBeginPointIdx = j;
-                int curEndPointIdx = j + 1 <= lastIdx ? j + 1 : 0;
+                int idx1 = j - 1 >= 0 ? j - 1 : lastIdx;
+                int idx2 = j;
+                int idx3 = j + 1 <= lastIdx ? j + 1 : 0;
+                int idx4 = idx3 + 1 <= lastIdx ? idx3 + 1 : 0;
 
-                Point beginPoint = listOfPoints.get(curBeginPointIdx);
-                Point endPoint = listOfPoints.get(curEndPointIdx);
+                Point p1 = listOfPoints.get(idx1);
+                Point p2 = listOfPoints.get(idx2);
+                Point p3 = listOfPoints.get(idx3);
+                Point p4 = listOfPoints.get(idx4);
 
-                double distance = Utils.getDistanceSqrt(beginPoint, endPoint);
+                double distance = getDistanceSqrt(p2, p3);
+                double firstAngle = getAngle(p1, p2, p3);
+                double secondAngle = getAngle(p2, p3, p4);
+
                 //ищем индекс начальной точки отрезка с минимальной длиной
-                if (distance < min_distance) {
+                if (distance < min_distance && 180 < (firstAngle + secondAngle)) {
                     min_distance = distance;
-                    beginPointIdx = curBeginPointIdx;
-                    endPointIdx = curEndPointIdx;
+
+                    point2idx = idx2;
+                    point3idx = idx3;
+
+                    point1 = p1;
+                    point2 = p2;
+                    point3 = p3;
+                    point4 = p4;
                 }
             }
-            //выделяем точки необходимые для нахождения пересечения, с учетом граничных случаев
-            Point p1 = listOfPoints.get(beginPointIdx);
-            int secondIdx = beginPointIdx - 1 >= 0 ? beginPointIdx - 1 : lastIdx;
-            Point p2 = listOfPoints.get(secondIdx);
-            Point p3 = listOfPoints.get(endPointIdx);
-            int fourthIdx = endPointIdx + 1 <= lastIdx ? endPointIdx + 1 : 0;
-            Point p4 = listOfPoints.get(fourthIdx);
-            Point newPoint = Utils.intersection(p1, p2, p3, p4);
+            Point newPoint = Utils.intersection(point1, point2, point3, point4);
             //точка пересечения не лежит на одной прямой с двумя другими точками, иначе ее можно просто удалить
             if (newPoint != null) {
-                listOfPoints.set(beginPointIdx, newPoint);
-                listOfPoints.remove(endPointIdx);
+                listOfPoints.set(point2idx, newPoint);
+                listOfPoints.remove(point3idx);
             } else {
-                listOfPoints.remove(beginPointIdx);
+                listOfPoints.remove(point2);
             }
         }
         return listOfPoints;
