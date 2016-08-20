f2abb2ec5e44e94e71b4285108c710f5f87ac3e4
Ruslan <vergilijus@ya.ru>
2016-08-20 23:05:06 +0300

    Обновлен command

--- src/main/java/com/technostart/playmate/core/common/Command.java
+++ src/main/java/com/technostart/playmate/core/common/Command.java
@@ -1,7 +1,6 @@
 package com.technostart.playmate.core.common;
 
-/**
- * Created by vergil on 18.08.16.
- */
-public interface Command {
+@FunctionalInterface
+public interface Command<T1, T2> {
+    T2 execute(T1 input);
 }
