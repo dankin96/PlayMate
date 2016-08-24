dfc2ae0d4b203cd100cf8cccd922c2203d8a2419
Ruslan <vergilijus@ya.ru>
2016-08-24 11:37:34 +0300

    Тест направления

--- src/test/java/com/technostart/playmate/core/cv/tracker/HitDetectorTest.java
+++ src/test/java/com/technostart/playmate/core/cv/tracker/HitDetectorTest.java
@@ -1,5 +1,6 @@
 package com.technostart.playmate.core.cv.tracker;
 
+import com.technostart.playmate.core.cv.tracker.Hit.Direction;
 import org.junit.Before;
 import org.junit.Test;
 import org.opencv.core.Core;
@@ -9,11 +10,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class HitDetectorTest {
     HitDetector hitDetector;
     List<Point> hitList;
+    List<Direction> directionList;
 
     @Before
     public void setUp() throws Exception {
@@ -21,11 +23,16 @@ public class HitDetectorTest {
 
         hitDetector = new HitDetector();
         hitList = new ArrayList<>();
-        hitDetector.setHitDetectorListener(hitPoint -> hitList.add(hitPoint));
+        directionList = new ArrayList<>();
+
+        hitDetector.setHitDetectorListener((hitPoint, direction) -> {
+            hitList.add(hitPoint);
+            directionList.add(direction);
+        });
     }
 
     @Test
-    public void testAddNewPoint() throws Exception {
+    public void testAddNewPoint() {
         hitDetector.addNewPoint(new Point(-2, 2));
         hitDetector.addNewPoint(new Point(-1, 1));
         hitDetector.addNewPoint(new Point(0, 0)); // hit
@@ -34,9 +41,13 @@ public class HitDetectorTest {
         hitDetector.addNewPoint(new Point(3, 1)); // hit
         hitDetector.addNewPoint(new Point(4, 2));
         hitDetector.addNewPoint(new Point(3, 3));
-        hitDetector.addNewPoint(new Point(2, 2));
+        hitDetector.addNewPoint(new Point(2, 2)); // hit
+        hitDetector.addNewPoint(new Point(1, 3));
+        hitDetector.addNewPoint(new Point(1, 3));
 
-        List<Point> expected = Arrays.asList(new Point(0, 0), new Point(3, 1));
-        assertEquals(expected, hitList);
+        List<Point> expectedPoints = Arrays.asList(new Point(0, 0), new Point(3, 1), new Point(2, 2));
+        List<Direction> expectedDirections = Arrays.asList(Direction.LEFT_TO_RIGHT, Direction.LEFT_TO_RIGHT, Direction.RIGHT_TO_LEFT);
+        assertEquals(expectedPoints, hitList);
+        assertEquals(expectedDirections, directionList);
     }
 }
